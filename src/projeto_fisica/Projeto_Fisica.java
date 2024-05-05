f0f9427bff9e1cb181df9b12e1a9561f2f32ed92
LuaBortko <133054603+LuaBortko@users.noreply.github.com>
2024-05-04 21:11:13 -0300

    Contas e Graficos

---
+++ src/projeto_fisica/Projeto_Fisica.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package projeto_fisica;
+
+import view.EntradasMenu;
+
+
+/**
+ *
+ * @author luana
+ */
+public class Projeto_Fisica {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {//L pa pb ni nf 
+        EntradasMenu e = new EntradasMenu();
+        e.setVisible(true);
+//        FuncaoDeOnda f = new FuncaoDeOnda(0.265e-9,0.0795e-9,0.159e-9,1,5,false);
+
+      
+        
+    }
+    
+}
