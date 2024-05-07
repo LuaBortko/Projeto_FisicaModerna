6c584fa315834644a2442d1c927e956174dbc507
LuaBortko <133054603+LuaBortko@users.noreply.github.com>
2024-05-07 18:17:35 -0300

    a

--- src/view/Visao.java
+++ src/view/Visao.java
@@ -157,6 +157,7 @@ public class Visao extends javax.swing.JFrame implements ActionListener{
     /**
      * @param args the command line arguments
      */
+    private int contadorFoton;
     private int xEmissao;
     private BufferedImage buffer;
     private Image foton;
